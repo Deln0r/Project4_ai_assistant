e28dfb95995bd816a6d3640670ffafdf175e5400
Ian Chechin <Delnor40rus@gmail.com>
2023-10-16 14:22:19 +0300

    ai

--- main.py
+++ main.py
@@ -2,52 +2,48 @@ import os
 import time
 import pyaudio
 import speech_recognition as sr
-import playsound 
+import playsound
 from gtts import gTTS
 import openai
+import uuid
 
-
-api_key = "Change this"
-
-lang ='en'
+api_key = "YOUR API HERE"
+lang = 'en'
 
 openai.api_key = api_key
-
 
 guy = ""
 
 while True:
-    def get_adio():
+    def get_audio():
         r = sr.Recognizer()
-        with sr.Microphone(device_index=1) as source:
+        with sr.Microphone() as source:
             audio = r.listen(source)
             said = ""
 
             try:
                 said = r.recognize_google(audio)
                 print(said)
-                global guy 
+                global guy
                 guy = said
-                
 
-                if "Friday" in said:
-                    words = said.split()
-                    new_string = ' '.join(words[1:])
-                    print(new_string) 
-                    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content":said}])
+                if "alexa" in said:
+                    new_string = said.replace("alexa", "")
+                    new_string = new_string.strip()
+                    print(new_string)
+                    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": new_string}])
                     text = completion.choices[0].message.content
-                    speech = gTTS(text = text, lang=lang, slow=False, tld="com.au")
-                    speech.save("welcome1.mp3")
-                    playsound.playsound("welcome1.mp3")
-                    
-            except Exception:
-                print("Exception")
+                    speech = gTTS(text=text, lang=lang, slow=False, tld="com.au")
+                    file_name = f"welcome_{str(uuid.uuid4())}.mp3"
+                    speech.save(file_name)
+                    playsound.playsound(file_name, block=False)
 
+            except Exception as e:
+                print(f"Exception: {str(e)}")
 
         return said
 
     if "stop" in guy:
         break
 
-
-    get_adio()
+    get_audio()
