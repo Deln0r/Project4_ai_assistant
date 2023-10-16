e905221757495297e337ac0e4eb5200bf3b06683
Ian Chechin <Delnor40rus@gmail.com>
2023-10-16 14:13:39 +0300

    16.10

--- main.py
+++ main.py
@@ -1,25 +1,53 @@
+import os
+import time
+import pyaudio
 import speech_recognition as sr
-import pyttsx3
-
-listener = sr.Recognizer()
-engine = pyttsx3.init()
-engine.say('Hello, I am Alexa')
-engine.say('How can I help you?')
-engine.runAndWait()
+import playsound 
+from gtts import gTTS
+import openai
 
 
-try:
-    with sr.Microphone() as source:
-        print('Listening...')
-        voice = listener.listen(source)
-        command = listener.recognize_google(voice)
-        command = command.lower()
-        if 'alexa' in command:
-            
-            print(command)
-except:
-    pass
+api_key = "Change this"
+
+lang ='en'
+
+openai.api_key = api_key
 
 
+guy = ""
 
-  gpt
+while True:
+    def get_adio():
+        r = sr.Recognizer()
+        with sr.Microphone(device_index=1) as source:
+            audio = r.listen(source)
+            said = ""
+
+            try:
+                said = r.recognize_google(audio)
+                print(said)
+                global guy 
+                guy = said
+                
+
+                if "Friday" in said:
+                    words = said.split()
+                    new_string = ' '.join(words[1:])
+                    print(new_string) 
+                    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content":said}])
+                    text = completion.choices[0].message.content
+                    speech = gTTS(text = text, lang=lang, slow=False, tld="com.au")
+                    speech.save("welcome1.mp3")
+                    playsound.playsound("welcome1.mp3")
+                    
+            except Exception:
+                print("Exception")
+
+
+        return said
+
+    if "stop" in guy:
+        break
+
+
+    get_adio()
